2b7d6eff52783baf0955972e91f77b80195264cd
Juliano Ramos <juliano.ramos@tog.design>
2024-05-14 13:20:34 -0300

    fundamentos do nest

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { Env } from '@/env'
+import { JwtStrategy } from './jwt.strategy'
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      global: true,
+      useFactory(config: ConfigService<Env, true>) {
+        const privateKey = config.get('JWT_PRIVATE_KEY')
+        const publicKey = config.get('JWT_PUBLIC_KEY')
+
+        return {
+          signOptions: { algorithm: 'RS256' },
+          privateKey: Buffer.from(privateKey, 'base64').toString(),
+          publicKey: Buffer.from(publicKey, 'base64').toString(),
+        }
+      },
+    }),
+  ],
+  providers: [JwtStrategy],
+})
+export class AuthModule {}
